ab1246c81c2c67861ab988f298478031ae469713
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-07-10 02:43:27 +0300

    Update and rename owner-add_del_owner.js to owner-اضافة_مالكين.js

--- plugins/owner-اضافة_مالكين.js
+++ plugins/owner-اضافة_مالكين.js
@@ -2,19 +2,19 @@
 const handler = async (m, { conn, text, args, usedPrefix, command }) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
   const tradutor = _translate.plugins.owner_add_del_owner
 
   const why = `${tradutor.texto1[0]} ${usedPrefix + command} @${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${tradutor.texto1[0]}`;
   const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
   if (!who) return conn.reply(m.chat, why, m, {mentions: [m.sender]});
   switch (command) {
-    case 'addowner':
+    case 'اضافة-مالك':
       const nuevoNumero = who;
       global.owner.push([nuevoNumero]);
       await conn.reply(m.chat, tradutor.texto2, m);
       break;
-    case 'delowner':
+    case 'حذف-مالك':
       const numeroAEliminar = who;
       const index = global.owner.findIndex(owner => owner[0] === numeroAEliminar);
       if (index !== -1) {
@@ -26,6 +26,6 @@ const handler = async (m, { conn, text, args, usedPrefix, command }) => {
       break;
   }
 };
-handler.command = /^(addowner|delowner)$/i;
+handler.command = /^(اضافة-مالك|حذف-مالك)$/i;
 handler.rowner = true;
 export default handler;
