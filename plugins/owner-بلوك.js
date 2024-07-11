eedb527dad4f1bb9cb0a9b8f639cc028c11df1a5
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-07-11 05:31:48 +0300

    Update and rename owner-block-unblock.js to owner-بلوك.js

--- plugins/owner-بلوك.js
+++ plugins/owner-بلوك.js
@@ -5,28 +5,34 @@ const handler = async (m, {text, conn, usedPrefix, command}) => {
   const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
   const tradutor = _translate.plugins.owner_block_unblock
 
-  const why = `${tradutor.texto1} ${usedPrefix + command} @${m.sender.split('@')[0]}*`;
+  const why = `❐┃منشن الي تبي تبلكه┃⛓️❯
+
+*⟐ مثال :*
+${usedPrefix + command} @${m.sender.split('@')[0]}`;
   const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
   if (!who) conn.reply(m.chat, why, m, {mentions: [m.sender]});
   const res = [];
   switch (command) {
     case 'blok': case 'block':
       if (who) {
-        await conn.updateBlockStatus(who, 'block').then(() => {
+        await conn.updateBlockStatus(who, 'بلوك').then(() => {
           res.push(who);
         });
       } else conn.reply(m.chat, why, m, {mentions: [m.sender]});
       break;
     case 'unblok': case 'unblock':
       if (who) {
-        await conn.updateBlockStatus(who, 'unblock').then(() => {
+        await conn.updateBlockStatus(who, 'فك-بلوك').then(() => {
           res.push(who);
         });
       } else conn.reply(m.chat, why, m, {mentions: [m.sender]});
       break;
   }
-  if (res[0]) conn.reply(m.chat, `${tradutor.texto2[0]} ${command} ${tradutor.texto2[1]} ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}*`, m, {mentions: res});
+  if (res[0]) conn.reply(m.chat, `❐┃اذا تبي تفك الباند┃⛓‍💥❯
+
+*⟐ مثال :*
+  ${command} ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}`, m, {mentions: res});
 };
-handler.command = /^(block|unblock)$/i;
+handler.command = /^(بلوك|فك-بلوك)$/i;
 handler.rowner = true;
 export default handler;
