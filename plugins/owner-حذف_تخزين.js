3d0fae5e0fad90a06ded148c537a4153996a753c
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-07-17 12:37:21 +0300

    Update and rename owner-delmsg.js to owner-حذف_تخزين.js

--- plugins/owner-حذف_تخزين.js
+++ plugins/owner-حذف_تخزين.js
@@ -15,6 +15,6 @@ const handler = async (m, {command, usedPrefix, text}) => {
 };
 handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map((v) => 'del' + v + ' <text>');
 handler.tags = ['database'];
-handler.command = /^eliminar(vn|msg|video|audio|img|sticker)$/;
+handler.command = /^حذفmsg$/;
 handler.rowner = true;
 export default handler;
