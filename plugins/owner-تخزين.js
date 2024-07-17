dd5adc62a40d0c53b0fe79318074a835573e251c
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-07-17 10:24:23 +0300

    Update and rename owner-رسائل.js to owner-تخزين.js

--- plugins/owner-تخزين.js
+++ plugins/owner-تخزين.js
@@ -9,11 +9,11 @@ const handler = async (m, {command, usedPrefix, text}) => {
   const M = m.constructor;
   const which = command.replace(/agregar/i, '');
   if (!m.quoted) throw tradutor.texto1;
-  if (!text) throw `${tradutor.texto2[0]} *${usedPrefix}list${which}* ${tradutor.texto2[1]}`;
+  if (!text) throw `${tradutor.texto2[0]} *${usedPrefix}list${which}* ةة ${tradutor.texto2[1]}`;
   const msgs = global.db.data.msgs;
-  if (text in msgs) throw `*[❗𝐈𝐍𝐅𝐎❗] '${text}' ${tradutor.texto3}`;
+  if (text in msgs) throw `*[❗𝐈𝐍𝐅𝐎❗] '${text}'لا ${tradutor.texto3}`;
   msgs[text] = M.toObject(await m.getQuotedObj());
-  m.reply(`${tradutor.texto4[0]} '${text}'${tradutor.texto4[1]} ${usedPrefix}ver${which} ${text}*`);
+  m.reply(`${tradutor.texto4[0]} '${text}'${tradutor.texto4[1]} ${usedPrefix}استدعاء${which} ${text}*`);
 };
 handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map((v) => 'add' + v + ' <text>');
 handler.tags = ['database'];
