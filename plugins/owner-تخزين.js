10927e9872f5b8e021857d77fc7add280658ebbe
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-07-17 10:43:43 +0300

    Update owner-تخزين.js

--- plugins/owner-تخزين.js
+++ plugins/owner-تخزين.js
@@ -9,14 +9,18 @@ const handler = async (m, {command, usedPrefix, text}) => {
   const M = m.constructor;
   const which = command.replace(/agregar/i, '');
   if (!m.quoted) throw tradutor.texto1;
-  if (!text) throw `${tradutor.texto2[0]} *${usedPrefix}list${which}* ةة ${tradutor.texto2[1]}`;
+  if (!text) throw `تت`;
   const msgs = global.db.data.msgs;
   if (text in msgs) throw `*[❗𝐈𝐍𝐅𝐎❗] '${text}'لا ${tradutor.texto3}`;
   msgs[text] = M.toObject(await m.getQuotedObj());
-  m.reply(`${tradutor.texto4[0]} '${text}'${tradutor.texto4[1]} ${usedPrefix}استدعاء${which} ${text}*`);
+  m.reply(`*❐┃تمت اضافة الرسالة الى الخزنة بنجاح┃🙂‍↕❯*
+*▢* لاستدعاء الرسالة المخزنة قم بكتابة استدعاء مع كلمة المرور
+
+*⟐ مثال :*
+.استدعاء <كلمة مرور>`);
 };
 handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map((v) => 'add' + v + ' <text>');
 handler.tags = ['database'];
-handler.command = /^اضافة(vn|msg|vid|aud|img|wm)$/;
+handler.command = /^تخزين$/;
 handler.rowner = true;
 export default handler;
