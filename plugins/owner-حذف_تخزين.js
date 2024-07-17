a3ad55a2696fb21db805c1d84196085b54cd69e1
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-07-17 13:02:21 +0300

    Update owner-حذف_تخزين.js

--- plugins/owner-حذف_تخزين.js
+++ plugins/owner-حذف_تخزين.js
@@ -7,11 +7,18 @@ const handler = async (m, {command, usedPrefix, text}) => {
   const tradutor = _translate.plugins.owner_delmsg
 
   const which = command.replace(/eliminar/i, '');
-  if (!text) throw `${tradutor.texto1[0]} ... ${usedPrefix}list${which} ${tradutor.texto1[1]}`;
+  if (!text) throw `*❐┃استخدام غير صحيح┃❗❯*
+*▢* لحذف احد الرسائل المخزنة استخدم الامر يليه كلمة المرور.
+*▢* اذا لا تتذكر كلمات المرور استخدم امر *قائمة-التخزين*
+
+*⟐ مثال :*
+‏.delmsg <كلمة المرور>`;
   const msgs = global.db.data.msgs;
-  if (!text in msgs) throw `${tradutor.texto2[0]} bbb '${text}' ${tradutor.texto2[1]}`;
+  if (!text in msgs) throw `${tradutor.texto2[0]} '${text}' ${tradutor.texto2[1]}`;
   delete msgs[text];
-  m.reply(`${ tradutor.texto3} uuu '${text}'*`);
+  m.reply(`*❐┃لقد تم الحذف بنجاح┃✅❯*
+
+*⟐ تم حذف جميع الرسائل التي باسم '${text}'*`);
 };
 handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map((v) => 'del' + v + ' <text>');
 handler.tags = ['database'];
