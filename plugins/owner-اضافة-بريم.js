3ec9ca2168b87a96f22eb060ca1ea45988f2e003
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-07-17 13:17:09 +0300

    Update owner-اضافة-بريم.js

--- plugins/owner-اضافة-بريم.js
+++ plugins/owner-اضافة-بريم.js
@@ -3,7 +3,7 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
+  const _translate = JSON.parse(fs.readFileSync(`./language/es.json`))
   const tradutor = _translate.plugins.owner_addprem
 
   let who;
@@ -67,7 +67,7 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
 };
 handler.help = ['addprem [@user] <days>'];
 handler.tags = ['owner'];
-handler.command = ['addprem', 'userpremium', 'addprem2', 'userpremium2', 'addprem3', 'userpremium3', 'addprem4', 'userpremium4'];
+handler.command = ['اضافة-بريم', 'userpremium', 'addprem2', 'userpremium2', 'addprem3', 'userpremium3', 'addprem4', 'userpremium4'];
 handler.group = true;
 handler.rowner = true;
 export default handler;
