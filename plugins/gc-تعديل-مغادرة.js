21c6c90dfe5f6cfeb865a04a629f36e4ff760be3
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-07-17 05:38:37 +0300

    Update gc-تعديل-مغادرة.js

--- plugins/gc-تعديل-مغادرة.js
+++ plugins/gc-تعديل-مغادرة.js
@@ -2,13 +2,22 @@
 const handler = async (m, {conn, text, isROwner, isOwner}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
+  const _translate = JSON.parse(fs.readFileSync(`./language/es.json`))
   const tradutor = _translate.plugins.gc_setbye
 
   if (text) {
     global.db.data.chats[m.chat].sBye = text;
     m.reply(tradutor.texto1);
-  } else throw `${tradutor.texto2}\n*- @user ${tradutor.texto3}`;
+  } else throw `*❐┃استخدام غير صحيح┃❗❯*
+*▢* لتعديل المغادرة يجب عليك كتابة ما يلي:
+
+*⟐ مثال :*
+*.تعديل-ترحيب < @user > للمنشن*
+*.تعديل-ترحيب < @group > اسم الجروب*
+*.تعديل-ترحيب < @desc > وصف الجروب*
+
+* *اسـتـخـدام الامـر :*
+.تعديل-مغادرة مع السلامة @user`;
 };
 handler.help = ['setbye <text>'];
 handler.tags = ['group'];
