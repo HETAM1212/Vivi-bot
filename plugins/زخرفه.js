131bef9b7f01938354d151e3a0a499d90dd95728
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-07-19 23:25:11 +0300

    Update زخرفه.js

--- plugins/زخرفه.js
+++ plugins/زخرفه.js
@@ -1,25 +1,24 @@
-import fetch from 'node-fetch'
-import { JSDOM } from 'jsdom'
-let handler = async (m, { conn, text }) => {
-    conn.reply(m.chat, Object.entries(await stylizeText(text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text)).map(([name, value]) => `*${name}*\n${value}`).join`\n\n`, m)
-}
-handler.help = ['styletext']
-handler.tags = ['tools']
-handler.command = /^زخرف$/i
+cmd({
+            pattern: "زخرفة",
+            desc: "Makes stylish/fancy given text",
+            category: "تحويل",
+            use: '56 Rin',
+            react: "✅",
+            filename: __filename
+        },
+        async(Void, citel, text) => {
+            if (isNaN(text.split(" ")[0]) || !text) {
+                let text = tiny(
+                    "زخرفة \n\مثال: .زخرفة 32 Rin\n\n"
+                );
+                listall("Rin").forEach((txt, num) => {
+                    text += `${(num += 1)} ${txt}\n`;
+                });
+                return await citel.reply(text);
+            }
 
+            let fancytextt = await fancytext(`${text.slice(2)}`, text.split(" ")[0])
+            citel.reply(fancytextt)
 
-export default handler
-
-async function stylizeText(text) {
-    let res = await fetch('http://qaz.wtf/u/convert.cgi?text=' + encodeURIComponent(text))
-    let html = await res.text()
-    let dom = new JSDOM(html)
-    let table = dom.window.document.querySelector('table').children[0].children
-    let obj = {}
-    for (let tr of table) {
-        let name = tr.querySelector('.aname').innerHTML
-        let content = tr.children[1].textContent.replace(/^\n/, '').replace(/\n$/, '')
-        obj[name + (obj[name] ? ' Reversed' : '')] = content
-    }
-    return obj
-}
+        }
+    )
