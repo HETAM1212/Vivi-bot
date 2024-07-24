25cbb84e255d6133ee247d7df946308bedc005eb
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-07-24 22:54:50 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -1457,7 +1457,7 @@ ${tradutor.texto1[1]} ${messageNumber}/3
         }
         m.isCommand = true;
         const xp = 'exp' in plugin ? parseInt(plugin.exp) : 20; // XP Earning per command
-        if (xp = 200) {
+        if (xp < 200) {
           m.reply('Ngecit -_-');
         } 
         // Hehehe
