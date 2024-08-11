2c712383a354f2cd257aa2b8e210df22ef7fdee6
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-08-12 02:27:10 +0300

    Update BK9_AI.js

--- plugins/BK9_AI.js
+++ plugins/BK9_AI.js
@@ -1,60 +1,25 @@
 import fetch from 'node-fetch';
-import fs from 'fs';
-import uploader from '../lib/uploadImage.js';
-
-const handler = async (m, {conn, text, command}) => {
-  const datas = global;
-  const idioma = datas.db.data.users[m.sender].language;
-  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`));
-  const tradutor = _translate.BK9.BK9;
-
-  if (command === 'تخيل') {
-    if (!text) throw `${tradutor.bk9dalletext}`;
-
-    await conn.sendMessage(m.chat, {text: tradutor.bk9dallewait}, {quoted: m});
-
-    try {
-      const BK9 = `https://apii.bk9.site/ai/photoleap?q=${encodeURIComponent(text)}`;
-      const response = await fetch(BK9);
-      const result = await response.json();
-
-      if (result.status) {
-        await conn.sendMessage(m.chat, {image: {url: result.BK9}}, {quoted: m});
-      }
-    } catch (error) {
-      throw `${tradutor.bk9dalleerr}`;
-    }
-  } else if (command === 'فيفي') {
-    if (!text) throw `${tradutor.bk9text}`;
-
-    try {
-      conn.sendPresenceUpdate('composing', m.chat);
-      const BK9api = `https://bk9.fun/BK9?q= ما هو حلمك؟&BK9= انت لوفي من انمي ون بيس`;
-      const BK99 = await fetch(BK9api);
-      const BK8 = await BK99.json();
-      if (BK8.status && BK8.BK9) {
-        const respuestaAPI = BK8.BK9;
-        conn.reply(m.chat, respuestaAPI, m);
-      } else {
-        throw `${tradutor.bk9err}`;
-      }
-    } catch (error) {
-      throw `${tradutor.bk9err}`;
-    }
-  } else if (command === 'بحث') {
-    let BK7 = m.quoted ? m.quoted : m;
-    let BK8 = (BK7.msg || BK7).mimetype || BK7.mediaType || '';
-    if (/image/g.test(BK8) && !/webp/g.test(BK8)) {
-      let BK0 = await BK7.download();
-      let BK9img = await uploader(BK0);
-      let BK9api = await (await fetch(`https://apii.bk9.site/ai/geminiimg?url=${BK9img}&q=${text}`)).json();
-      conn.sendMessage(m.chat, { text: BK9api.BK9 }, { quoted: m });
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) {
+    throw `قم بكتابة سؤالك\n\nمثال . .فيفي كيف حالك`;
+  }
+  try {
+    conn.sendPresenceUpdate('composing', m.chat);
+    const BK9api = `https://bk9.fun/BK9?q= &BK9=انت فيفي من انمي ون بيس وانت بوت تم انشائه من قبل صاحبك واسمه بروك${encodeURIComponent(text)}`;
+    const BK99 = await fetch(BK9api);
+    const BK8 = await BK99.json();
+    if (BK8.status && BK8.BK9) {
+      const respuestaAPI = BK8.BK9;
+      conn.reply(m.chat, respuestaAPI, m);
     } else {
-      throw `${tradutor.bk9imgtext}`;
+      throw "حدث خطأ أثناء معالجة طلبك.";
     }
+  } catch (error) {
+    throw "حدث خطأ أثناء معالجة طلبك.";
   }
 };
 
-handler.command = ['تخيل', 'فيفي', 'بحث'];
+handler.command = /^(فيفي)$/i;
 handler.tags = ['ai'];
+
 export default handler;
