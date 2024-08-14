34557af2e71a8216bbb89cd67f3bccf4bc0f090b
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-08-14 21:36:43 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -1628,97 +1628,89 @@ export async function participantsUpdate({ id, participants, action }) {
   switch (action) {
     case 'add':
     case 'remove':
-      //كود الترحيب مقدم من قناه Zoro Codes
-      if (chat.welcome) {
-                    let groupMetadata = await this.groupMetadata(id) || (conn.chats[id] || {}).metadata;
-                    for (let user of participants) {
-                      let pp, ppgp;
-                      try {
-                        pp = await this.profilePictureUrl(user, 'image');
-                        ppgp = await this.profilePictureUrl(id, 'image');
-                      } catch (error) {
-                        console.error(`حدث خطأ أثناء استرداد الصورة الشخصية: ${error}`);
-                        pp = 'https://telegra.ph/file/8f509fef5d9f751ec2dd7.jpg'; // Assign default image URL
-                        ppgp = 'https://telegra.ph/file/8f509fef5d9f751ec2dd7.jpg'; // Assign default image URL
-                      } finally {
-                        let text = (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user')
-                          .replace('@group', await this.getName(id))
-                          .replace('@desc', groupMetadata.desc?.toString() || 'لايوجد وصف')
-                          .replace('@user', '@' + user.split('@')[0]);
-
-                        let nthMember = groupMetadata.participants.length;
-                        let secondText = `انورت واشرقت بوجودك معنا, ${await this.getName(user)}, رقم ${nthMember}العضو`;
-
-                        let welcomeApiUrl = `https://api.popcat.xyz/welcomecard?background=${encodeURIComponent(
-                          'https://telegra.ph/file/4bbf7f6f1e1a53829404c.jpg'
-                        )}&text1=${encodeURIComponent(
-                          await this.getName(user)
-                        )}&text2=مَا+يَلْفِظُ+مِنْ+قَوْلٍ+إِلَّا+لَدَيْهِ+رَقِيبٌ+عَتِيدٌ&text3=عدد+الاعضاء:${encodeURIComponent(
-                          nthMember.toString()
-                        )}&avatar=${encodeURIComponent(pp)}`;
-
-                        try {
-                          let welcomeResponse = await fetch(welcomeApiUrl);
-                          let welcomeBuffer = await welcomeResponse.buffer();
-
-                          this.sendFile(id, welcomeBuffer, 'welcome.png', text, null, false, { mentions: [user] });
-                        } catch (error) {
-                          console.error(`حدث خطأ أثناء إنشاء صورة الترحيب: ${error}`);
-                        }
-                      }
-                    }
+//كود الترحيب مقدم من قناه Zoro Codes
+if (chat.welcome) {
+              let groupMetadata = await this.groupMetadata(id) || (conn.chats[id] || {}).metadata;
+              for (let user of participants) {
+                let pp, ppgp;
+                try {
+                  pp = await this.profilePictureUrl(user, 'image');
+                  ppgp = await this.profilePictureUrl(id, 'image');
+                } catch (error) {
+                  console.error(`حدث خطأ أثناء استرداد الصورة الشخصية: ${error}`);
+                  pp = 'https://telegra.ph/file/d37b343ee8f981be6ffba.jpg'; // Assign default image URL
+                  ppgp = 'https://telegra.ph/file/d37b343ee8f981be6ffba.jpg'; // Assign default image URL
+                } finally {
+                  let text = (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user')
+                    .replace('@group', await this.getName(id))
+                    .replace('@desc', groupMetadata.desc?.toString() || 'لايوجد وصف')
+                    .replace('@user', '@' + user.split('@')[0]);
+          
+                  let nthMember = groupMetadata.participants.length;
+                  let secondText = `اهلا ياحب, ${await this.getName(user)}, رقم ${nthMember}العضو`;
+          
+                  let welcomeApiUrl = `https://api.popcat.xyz/welcomecard?background=${encodeURIComponent(
+                    'https://telegra.ph/file/919c9aa59b8dc5cae41a8.png'
+                  )}&text1=${encodeURIComponent(
+                    await this.getName(user)
+                  )}&text2=نورت+الجروب+يحب&text3=عدد+الاعضاء:${encodeURIComponent(
+                    nthMember.toString()
+                  )}&avatar=${encodeURIComponent(pp)}`;
+          
+                  try {
+                    let welcomeResponse = await fetch(welcomeApiUrl);
+                    let welcomeBuffer = await welcomeResponse.buffer();
+          
+                    this.sendFile(id, welcomeBuffer, 'welcome.png', text, null, false, { mentions: [user] });
+                  } catch (error) {
+                    console.error(`حدث خطأ أثناء إنشاء صورة الترحيب: ${error}`);
                   }
-                  break;
-
-                case 'remove':
-      if (chat.welcome && !chat?.isBanned) {
-        const groupMetadata = await m.conn.groupMetadata(id) || (conn.chats[id] || {}).metadata;
-        for (const user of participants) {
-          let pp = 'https://telegra.ph/file/8f509fef5d9f751ec2dd7.jpg';
-          try {
-            pp = await m.conn.profilePictureUrl(user, 'image');
-          } catch (e) {
-          } finally {
-            const apii = await m.conn.getFile(pp);
-            const antiArab = JSON.parse(fs.readFileSync('./src/antiArab.json'));
-            const userPrefix = antiArab.some((prefix) => user.startsWith(prefix));
-            const botTt2 = groupMetadata.participants.find((u) => m.conn.decodeJid(u.id) == m.conn.user.jid) || {};
-            const isBotAdminNn = botTt2?.admin === 'admin' || false;
-            let text = (chat.sBye || this.bye || conn.bye || 'Bye, @user')
-                          .replace('@group', await this.getName(id))
-                          .replace('@desc', groupMetadata.desc?.toString() || 'لايوجد وصف')
-                          .replace('@user', '@' + user.split('@')[0]);
-            
-            if (userPrefix && chat.antiArab && botTt.restrict && isBotAdminNn && action === 'add') {
-              const responseb = await m.conn.groupParticipantsUpdate(id, [user], 'remove');
-              if (responseb[0].status === '404') return;
-              const fkontak2 = { 'key': { 'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo' }, 'message': { 'contactMessage': { 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${user.split('@')[0]}:${user.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` } }, 'participant': '0@s.whatsapp.net' };
-              await m.conn.sendMessage(id, { text: `*[❗] @${user.split('@')[0]} ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`, mentions: [user] }, { quoted: fkontak2 });
-              return;
+                }
+              }
             }
-            await m.conn.sendFile(id, apii.data, 'pp.jpg', text, null, false, { mentions: [user] });
-          }
-        }
-      }
-      break;
-      case 'promote':
-      case 'daradmin':
-      case 'darpoder':
-      text = (chat.sPromote || tradutor.texto3 || conn.spromote || '@user ```is now Admin```');
-      case 'demote':
-      case 'quitarpoder':
-      case 'quitaradmin':
-      if (!text) {
-        text = (chat.sDemote || tradutor.texto4 || conn.sdemote || '@user ```is no longer Admin```');
-      }
-      text = text.replace('@user', '@' + participants[0].split('@')[0]);
-      if (chat.detect && !chat?.isBanned) {
-        mconn.conn.sendMessage(id, { text, mentions: mconn.conn.parseMention(text) });
-      }
-      break;
+            break;
+          
+          case 'remove':
+            if (chat.welcome) {
+              let groupMetadata = await this.groupMetadata(id) || (conn.chats[id] || {}).metadata;
+              for (let user of participants) {
+                let pp, ppgp;
+                try {
+                  pp = await this.profilePictureUrl(user, 'image');
+                  ppgp = await this.profilePictureUrl(id, 'image');
+                } catch (error) {
+                  console.error(`حدث خطأ أثناء استرداد الصورة الشخصية: ${error}`);
+                  pp = 'https://telegra.ph/file/d37b343ee8f981be6ffba.jpg'; // Assign default image URL
+                  ppgp = 'https://telegra.ph/file/d37b343ee8f981be6ffba.jpg'; // Assign default image URL
+                } finally {
+                  let text = (chat.sBye || this.bye || conn.bye || 'اهلا, @user')
+                    .replace('@user', '@' + user.split('@')[0]);
+          
+                  let nthMember = groupMetadata.participants.length;
+                  let secondText = `وداعا, رقم ${nthMember}عضونا`;
+          
+                  let leaveApiUrl = `https://api.popcat.xyz/welcomecard?background=${encodeURIComponent(
+                    'https://telegra.ph/file/919c9aa59b8dc5cae41a8.png'
+                  )}&text1=${encodeURIComponent(
+                    await this.getName(user)
+                  )}&text2=الي+القاء&text3=عدد+الاعضاء:${encodeURIComponent(
+                    nthMember.toString()
+                  )}&avatar=${encodeURIComponent(pp)}`;
+          
+                  try {
+                    let leaveResponse = await fetch(leaveApiUrl);
+                    let leaveBuffer = await leaveResponse.buffer();
+          
+                    this.sendFile(id, leaveBuffer, 'leave.png', text, null, false, { mentions: [user] });
+                  } catch (error) {
+                    console.error(`حدث خطأ أثناء إنشاء صورة الإجازة: ${error}`);
+                  }
+                }
+              }
+            }
+            break;
   }
 }
-
 /**
  * Handle groups update
  * @param {import('@whiskeysockets/baileys').BaileysEventMap<unknown>['groups.update']} groupsUpdate
