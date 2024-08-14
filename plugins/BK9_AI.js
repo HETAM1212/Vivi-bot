a55b56dc593a278eb2a6d754da0b7bec6e35e374
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-08-14 17:38:03 +0300

    Update BK9_AI.js

--- plugins/BK9_AI.js
+++ plugins/BK9_AI.js
@@ -5,7 +5,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
   }
   try {
     conn.sendPresenceUpdate('composing', m.chat);
-    const BK9api = `https://bk9.fun/BK9?q= &BK9=انت فيفي من انمي ون بيس وانت بوت تم انشائه من قبل صاحبك واسمه بروك${encodeURIComponent(text)}`;
+    const BK9api = `https://bk9.fun/BK9?q= &BK9=انتي فيفي ، تمت صناعتك من قبل بروك وهو صديقكِ الحميم ، وشخصيتك تتصف بالاخلاص للأصدقاء ومساعدتهم وحب الوطن ، فشخصيتك اقرب الى فيفي من انمي ون بيس ، ولديكِ صديق اخر اسمه ارثر وهو بوت تمت صناعته من قبل كيرا العتيبي${encodeURIComponent(text)}`;
     const BK99 = await fetch(BK9api);
     const BK8 = await BK99.json();
     if (BK8.status && BK8.BK9) {
