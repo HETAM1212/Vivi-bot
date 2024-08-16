ad0cd39025c30220d41f25a5a54308d4d2749e00
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-08-16 03:38:24 +0300

    Update and rename owner-reporte.js to owner-ابلاغ.js

--- plugins/owner-ابلاغ.js
+++ plugins/owner-ابلاغ.js
@@ -10,11 +10,11 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (text.length < 10) throw tradutor.texto2;
   if (text.length > 1000) throw tradutor.texto3;
   const teks = `${tradutor.texto4[0]} wa.me/${m.sender.split`@`[0]}\n${tradutor.texto4[1]} ${text}\n*┴*`;
-  conn.reply('5219992095479@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
-  conn.reply('584125778026@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('96551048712@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('96551048712@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
   m.reply(tradutor.texto5);
 };
 handler.help = ['reporte', 'request'].map((v) => v + ' <teks>');
 handler.tags = ['info'];
-handler.command = /^(report|request|reporte|bugs|bug|report-owner|reportes)$/i;
+handler.command = /^(report|ابلاغ|شكوى|مشكله|bug|report-owner|reportes)$/i;
 export default handler;
