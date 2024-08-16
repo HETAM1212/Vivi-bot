bd91da9d0c79dfc7a5f5a9d4a87d33574e29bf11
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-08-16 04:19:36 +0300

    Update owner-ابلاغ.js

--- plugins/owner-ابلاغ.js
+++ plugins/owner-ابلاغ.js
@@ -1,12 +1,14 @@
-
-
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
   const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
   const tradutor = _translate.plugins.owner_reporte
 
-  if (!text) throw `${tradutor.texto1[0]}\n*${usedPrefix + command} ${tradutor.texto1[1]} ${usedPrefix}play ${tradutor.texto1[2]}`;
+  if (!text) throw `*❐┃استخدام الامر غير صحيح┃❗❯*
+اكتب المشكلة التي تواجهها او بلاغك 
+
+*⟐ مثال :*
+.بلاغ < اكتب المشكلة >`;
   if (text.length < 10) throw tradutor.texto2;
   if (text.length > 1000) throw tradutor.texto3;
   const teks = `${tradutor.texto4[0]} wa.me/${m.sender.split`@`[0]}\n${tradutor.texto4[1]} ${text}\n*┴*`;
