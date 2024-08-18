f38bf2495a8f66607667d2e73c1ce7c06b97336f
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-08-18 12:56:01 +0300

    Create game-احزر1.js

---
+++ plugins/game-احزر1.js
@@ -0,0 +1,43 @@
+import fs from 'fs';
+
+let timeout = 30000;
+let poin = 2000;
+
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tekateki = conn.tekateki ? conn.tekateki : {};
+    let id = m.chat;
+    if (id in conn.tekateki) {
+        conn.reply(m.chat, '*❐┃لم يتم الاجابة على السؤال بعد┃❗❯*', conn.tekateki[id][0]);
+        throw false;
+    }
+    let tekateki = JSON.parse(fs.readFileSync(`./src/game/احزر.json`));
+    let json = tekateki[Math.floor(Math.random() * tekateki.length)];
+    let _clue = json.response;
+    let clue = _clue.replace(/[A-Za-z]/g, ''); // Fixed this line
+    let caption = `
+╮━─━═⟐ ❪╰احزر╮❫ ⟐═━─━╭
+*● ${json.question}*
+ *◄ الـوقـت : ${(timeout / 1000).toFixed(2)} ثانيه*
+ *◄ الـجـائزة : ${poin} XP*
+╯━───━═⟐ ❈ ⟐═━───━╰
+> *الرد على هذه الرسالة للإجابة*
+`.trim();
+    conn.tekateki[id] = [
+       await conn.reply(m.chat, caption, m),
+        json, poin,
+        setTimeout(async () => {
+            if (conn.tekateki[id]) await conn.reply(m.chat, `
+╮━─━═⟐ ❪╰احزر╮❫ ⟐═━─━╭
+*◄ انتهى وقت الاجابة ⌛*
+*◄ الاجابة : ${json.response}*
+╯━───━═⟐ ❈ ⟐═━───━╰`, conn.tekateki[id][0]);
+            delete conn.tekateki[id];
+        }, timeout)
+    ];
+};
+
+handler.help = ['miku'];
+handler.tags = ['game'];
+handler.command = /^(احزر|لغز)$/i;
+
+export default handler;
