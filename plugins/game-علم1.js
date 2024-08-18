450bd655b2ca8f4fa61949af9ddd1f9827f5f973
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-08-18 20:21:30 +0300

    Update game-علم1.js

--- plugins/game-علم1.js
+++ plugins/game-علم1.js
@@ -2,10 +2,10 @@ import fetch from 'node-fetch'
 let timeout = 30000
 let poin = 2000
 let handler = async (m, { conn, command, usedPrefix }) => {
-    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    conn.Alamvivi = conn.Alamvivi ? conn.Alamvivi : {}
     let id = m.chat
-    if (id in conn.tebakbendera) {
-        conn.reply(m.chat, '*❐╏لم يتم الاجابة علي السؤال بعد┃❌ ❯*', conn.tebakbendera[id][0])
+    if (id in conn.Alamvivi) {
+        conn.reply(m.chat, '*❐╏لم يتم الاجابة علي السؤال بعد┃❌ ❯*', conn.Alamvivi[id][0])
         throw false
     }
     let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/799d5646ceed992bf862026847473852/raw/dcbecff259b1d94615d7c48079ed1396ed42ef67/gistfile1.txt')).json()
@@ -18,12 +18,12 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 *▢❯* الرد على هذه الرسالة مع الاجابة!
 ╯┈──┈┈─┈┈┈┈──┈┈─┈⟐
     `.trim()
-    conn.tebakbendera[id] = [
+    conn.Alamvivi[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `*❮ ⌛┇انتهي الوقت┇⌛❯*\n*❐↞┇الاجـابـة✅↞ ${json.name}┇*`, conn.tebakbendera[id][0])
-            delete conn.tebakbendera[id]
+            if (conn.Alamvivi[id]) conn.reply(m.chat, `*❮ ⌛┇انتهي الوقت┇⌛❯*\n*❐↞┇الاجـابـة✅↞ ${json.name}┇*`, conn.Alamvivi[id][0])
+            delete conn.Alamvivi[id]
         }, timeout)
     ]
 }
