c430cbc744257df32d71dd307fbcced8ff1c66c1
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-08-18 17:04:05 +0300

    Update and rename game-علم.js to game-علم1.js

--- plugins/game-علم1.js
+++ plugins/game-علم1.js
@@ -1,31 +1,28 @@
 import fetch from 'node-fetch'
-let timeout = 60000
-let poin = 4999
+let timeout = 30000
+let poin = 2000
 let handler = async (m, { conn, command, usedPrefix }) => {
     conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
     if (id in conn.tebakbendera) {
-        conn.reply(m.chat, '*جاوب علي السؤال السابق اولا*', conn.tebakbendera[id][0])
+        conn.reply(m.chat, '*❐╏لم يتم الاجابة علي السؤال بعد┃❌ ❯*', conn.tebakbendera[id][0])
         throw false
     }
     let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/799d5646ceed992bf862026847473852/raw/dcbecff259b1d94615d7c48079ed1396ed42ef67/gistfile1.txt')).json()
   let json = src[Math.floor(Math.random() * src.length)]
     let caption = `
-*⟣┈┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢*
-*⌬ ❛╏ ${command.toUpperCase()}*
-*⟣┈┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢*
-*الوقت* *${(timeout / 1000).toFixed(2)} ثانيه*
-*استخدم انسحب للانسحاب*
-*الجائزه: ${poin} XP*
-*⟣┈┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢*
-   *⌬ ❛╏ by: 𝑧ₑ𝑧ₒ_𝑏ₒ𝑡*
-*⟣┈┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢*
+╮─┈〈 *تـخـمـيـن الـعـلم 🏳️* 〉┈─⟐
+*▢❯* الوقت : *${(timeout / 1000).toFixed(2)}* ثانيه
+*▢❯* اكتب :
+*▢❯* الجائزة : *${poin}* EXP
+*▢❯* الرد على هذه الرسالة مع الاجابة!
+╯┈──┈┈─┈┈┈┈──┈┈─┈⟐
     `.trim()
     conn.tebakbendera[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `*انتهي الوقت!*\n*الاجابه هيا* *${json.name}*`, conn.tebakbendera[id][0])
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `*❮ ⌛┇انتهي الوقت┇⌛❯*\n*❐↞┇الاجـابـة✅↞ ${json.name}┇*`, conn.tebakbendera[id][0])
             delete conn.tebakbendera[id]
         }, timeout)
     ]
