586914230e421cf58c0a13a80a98509ce13618f7
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-08-19 05:56:49 +0300

    Update ادوات-رابطي.js

--- plugins/ادوات-رابطي.js
+++ plugins/ادوات-رابطي.js
@@ -1,14 +1,12 @@
-// مقدم من قناة aboud coding 
- // https://whatsapp.com/channel/0029VaI4a3JDZ4LbOAHbdu47
 let handler = async (m, { conn, text }) => {
-  let tagme = `https://wa.me/+${m.sender.replace(`+`)}/?text=‏`
+  let tagme = `https://wa.me/+${m.sender.replace(`+`)}/?text=ViviBot`
   let mylink = [m.sender]
   conn.reply(m.chat, tagme, m, { contextInfo: { mylink }})
 }
-handler.help = ['يرسل رابطك']
+handler.help = ['رابطي']
 handler.tags = ['group']
 handler.command = /^رابطي$/i
 
-handler.group = false
+handler.group = true
 
 export default handler
