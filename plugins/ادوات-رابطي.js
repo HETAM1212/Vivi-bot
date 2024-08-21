cbb151a773740798997819b6456815659be3d00e
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-08-21 12:03:04 +0300

    Update ادوات-رقمي.js

--- plugins/ادوات-رابطي.js
+++ plugins/ادوات-رابطي.js
@@ -1,12 +1,14 @@
+// مقدم من قناة aboud coding 
+ // https://whatsapp.com/channel/0029VaI4a3JDZ4LbOAHbdu47
 let handler = async (m, { conn, text }) => {
-  let tagme = `https://wa.me/+${m.sender.replace(`+`)}/?text=ViviBot`
+  let tagme = `https://wa.me/+${m.sender.replace(`+`)}/?text=السلام-عليكم`
   let mylink = [m.sender]
   conn.reply(m.chat, tagme, m, { contextInfo: { mylink }})
 }
-handler.help = ['رابطي']
+handler.help = ['يرسل رابطك']
 handler.tags = ['group']
-handler.command = /^رابطي$/i
+handler.command = /^رقمي$/i
 
-handler.group = true
+handler.group = false
 
 export default handler
